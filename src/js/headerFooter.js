bda572d424d4dfdbed96a8b99dce7e6ad9f5de9d
Awinel <benjamin.huerta.awi@gmail.com>
2023-07-11 10:46:56 -0400

    Test09

--- src/js/headerFooter.js
+++ src/js/headerFooter.js
@@ -2,13 +2,13 @@ import { loader } from "./utils.js";
 
 export function renderloader() {
     const headerSelector = ".main-header";
-    const headerPath = "/src/partials/header.html";
+    const headerPath = "/personalproject/src/partials/header.html";
 
     const footerSelector = ".main-footer";
-    const footerPath = "/src/partials/footer.html";
+    const footerPath = "/personalproject/src/partials/footer.html";
 
     const navSelector = ".navbar-menu";
-    const navPath = "/src/partials/nav.html";
+    const navPath = "/personalproject/src/partials/nav.html";
 
     loader(headerPath, headerSelector);
     loader(footerPath, footerSelector);
