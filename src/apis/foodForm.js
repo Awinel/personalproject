b225223d373e4843d7c3cde4827e1fd9ed9fdc71
Awinel <benjamin.huerta.awi@gmail.com>
2023-07-16 22:46:26 -0400

    Food form and localStorage
    Creation of the localStorage, get and set and also some of the script in the foodForm.

--- src/apis/foodForm.js
+++ src/apis/foodForm.js
@@ -1,17 +1,20 @@
-import { getElement } from "../js/utils.js";
+import { getElement, getLocalStorage, setLocalStorage } from "../js/utils.js";
 
 export async function setupFoodForm() {
   function foodForm() {
     return `
       <form class="foodForm">
         <label for="ateFood">Enter the food you ate: </label>
-        <input type="text" id="ateFood" name="ateFood" required value="banana">
+        <input type="text" id="ateFood" name="ateFood" required>
         <input type="submit" value="submit">
       </form>
       <h2>Food eaten</h2>
       <div class="foodEaten"></div>
     `;
   }
+
+  const foodArray =[]
+  const ateFood = getLocalStorage("ateFood");
 
   const formContainer = document.querySelector('.foodForm');
   formContainer.innerHTML = foodForm();
@@ -21,9 +24,12 @@ export async function setupFoodForm() {
 
   form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
-
+    
+    
     const input = document.querySelector('#ateFood');
     const food = input.value;
+    foodArray.push(food);
+
 
     const paragraph = document.createElement('p');
     paragraph.textContent = food;
