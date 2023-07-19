e3e5663b04ab8e370c36399e4d2da2f17ee47c84
Awinel <benjamin.huerta.awi@gmail.com>
2023-07-19 18:21:13 -0400

    FoodForm and css styled
    I style the css and some of the foodForm.js to look better.

--- src/apis/foodForm.js
+++ src/apis/foodForm.js
@@ -3,13 +3,17 @@ import { getElement, getLocalStorage, setLocalStorage } from "../js/utils.js";
 export async function setupFoodForm() {
   function foodForm() {
     return `
+
+      <h2>Food eaten</h2>
+      <div class="foodEaten"></div>
+
       <form class="foodForm">
+        
         <label for="ateFood">Enter the food you ate: </label>
         <input type="text" id="ateFood" name="ateFood" required>
         <input type="submit" value="submit">
       </form>
-      <h2>Food eaten</h2>
-      <div class="foodEaten"></div>
+
     `;
   }
 
